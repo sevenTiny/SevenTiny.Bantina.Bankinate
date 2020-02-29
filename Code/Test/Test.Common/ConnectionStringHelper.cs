86aa1fc4641cbfa82bb827e7a4355c8132ea6e68
seventiny <seventiny@foxmail.com>
2020-02-29 17:35:11 +0800

    add test

--- Code/Test/Test.Common/ConnectionStringHelper.cs
+++ Code/Test/Test.Common/ConnectionStringHelper.cs
@@ -2,7 +2,7 @@
 {
     public class ConnectionStringHelper
     {
-        public static string ConnectionString_Write_MySql = "server=127.0.0.1;Port=3306;database=SevenTinyTest;uid=sa;pwd=123456;Allow User Variables=true;SslMode=none;";
+        public static string ConnectionString_Write_MySql = "server=192.168.0.107;Port=39901;database=SevenTinyTest;uid=root;pwd=123456;Allow User Variables=true;SslMode=none;";
         public static string[] ConnectionStrings_Read_MySql = new[] { ConnectionString_Write_MySql, ConnectionString_Write_MySql, ConnectionString_Write_MySql };
 
         public static string ConnectionString_Write_SqlServer = "Data Source=127.0.0.1;Initial Catalog=SevenTinyTest;User ID=sa;Password=123456;MultipleActiveResultSets=True;";
--- Code/Test/Test.MySql/PersistenceTest.cs
+++ Code/Test/Test.MySql/PersistenceTest.cs
@@ -1,150 +1,180 @@
-﻿//using SevenTiny.Bantina.Bankinate;
-//using SevenTiny.Bantina.Bankinate.Attributes;
-//using System;
-//using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.Linq;
-//using Test.Common;
-//using Test.Common.Model;
-//using Xunit;
+﻿using SevenTiny.Bantina.Bankinate;
+using SevenTiny.Bantina.Bankinate.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Test.Common;
+using Test.Common.Model;
+using Xunit;
 
-//namespace Test.MySql
-//{
-//    /// <summary>
-//    /// 数据预置类
-//    /// </summary>
-//    public class PersistenceTest
-//    {
-//        [DataBase("SevenTinyTest")]
-//        private class DataPreseterDb : MySqlDbContext<DataPreseterDb>
-//        {
-//            public DataPreseterDb() : base(ConnectionStringHelper.ConnectionString_Write, ConnectionStringHelper.ConnectionStrings_Read)
-//            {
+namespace Test.MySql
+{
+    /// <summary>
+    /// 数据预置类
+    /// </summary>
+    public class PersistenceTest
+    {
+        [DataBase("SevenTinyTest")]
+        private class DataPreseterDb : MySqlDbContext<DataPreseterDb>
+        {
+            public DataPreseterDb() : base(ConnectionStringHelper.ConnectionString_Write_MySql, ConnectionStringHelper.ConnectionStrings_Read_MySql)
+            {
 
-//            }
-//        }
+            }
+        }
 
-//        //[Fact]
-//        [Trait("desc", "初始化测试数据")]
-//        public void InitData()
-//        {
-//            return;
-//            using (var db = new DataPreseterDb())
-//            {
-//                //清空所有数据,并重置索引
-//                db.ExecuteSql("truncate table " + db.GetTableName<OperationTest>());
+        //[Fact]
+        public void InitData_OperationTest()
+        {
+            using (var db = new DataPreseterDb())
+            {
+                //清空所有数据,并重置索引
+                db.ExecuteSql("truncate table " + db.GetTableName<OperationTest>());
 
-//                //预置测试数据
-//                List<OperationTest> models = new List<OperationTest>();
-//                for (int i = 1; i < 1001; i++)
-//                {
-//                    db.Add<OperationTest>(new OperationTest
-//                    {
-//                        Key2 = i,
-//                        StringKey = $"test_{i}",
-//                        IntKey = i,
-//                        IntNullKey = null,
-//                        DateNullKey = DateTime.Now.Date,
-//                        DateTimeNullKey = DateTime.Now,
-//                        DoubleNullKey = i,
-//                        FloatNullKey = i
-//                    });
-//                }
-//            }
-//            Assert.True(true);
-//        }
+                //预置测试数据
+                List<OperationTest> models = new List<OperationTest>();
+                for (int i = 1; i < 1001; i++)
+                {
+                    db.Add<OperationTest>(new OperationTest
+                    {
+                        Key2 = i,
+                        StringKey = $"test_{i}",
+                        IntKey = i,
+                        IntNullKey = null,
+                        DateNullKey = DateTime.Now.Date,
+                        DateTimeNullKey = DateTime.Now,
+                        DoubleNullKey = i,
+                        FloatNullKey = i
+                    });
+                }
+            }
+        }
 
-//        [Fact]
-//        [Description("持久化测试")]
-//        public void Persistence()
-//        {
-//            using (var db = new DataPreseterDb())
-//            {
-//                int value = 999999;
+        //[Fact]
+        public void InitData_OperationTest2()
+        {
+            using (var db = new DataPreseterDb())
+            {
+                //清空所有数据,并重置索引
+                db.ExecuteSql("truncate table " + db.GetTableName<OperationTest2>());
 
-//                //初次查询没有数据
-//                db.Queryable<OperationTest>().Where(t => t.StringKey.StartsWith("AddTest")).ToList();
+                //预置测试数据
+                List<OperationTest2> models = new List<OperationTest2>();
+                for (int i = 1; i < 10000; i++)
+                {
+                    models.Add(new OperationTest2
+                    {
+                        Uid = Guid.NewGuid(),
+                        StringKey = string.Concat("str_", i)
+                    });
+                }
 
-//                //add一条数据
-//                OperationTest model = new OperationTest
-//                {
-//                    IntKey = value,
-//                    StringKey = "AddTest"
-//                };
-//                model.IntKey = value;
-//                db.Add<OperationTest>(model);
+                db.Add<OperationTest2>(models);
+            }
+        }
 
-//                //插入后查询有一条记录
-//                var re1 = db.Queryable<OperationTest>().Where(t => t.StringKey.StartsWith("AddTest")).ToList();
-//                Assert.Single(re1);
-//                Assert.Equal(value, re1.First().IntKey);
+        //[Fact]
+        //[Description("持久化测试")]
+        //public void Persistence()
+        //{
+        //    using (var db = new DataPreseterDb())
+        //    {
+        //        int value = 999999;
 
-//                //查询一条
-//                var entity = db.Queryable<OperationTest>().Where(t => t.IntKey == value).FirstOrDefault();
-//                Assert.NotNull(entity);
-//                Assert.Equal(value, entity.IntKey);
+        //        //初次查询没有数据
+        //        db.Queryable<OperationTest>().Where(t => t.StringKey.StartsWith("AddTest")).ToList();
 
-//                //更新数据
-//                //entity.Id = value;   //自增的主键不应该被修改,如果用这种方式进行修改，给Id赋值就会导致修改不成功，因为条件是用第一个主键作为标识修改的
-//                entity.Key2 = value;
-//                entity.StringKey = $"UpdateTest_{value}";
-//                entity.IntNullKey = value;
-//                entity.DateTimeNullKey = DateTime.Now;
-//                entity.DateNullKey = DateTime.Now.Date;
-//                entity.DoubleNullKey = entity.IntNullKey;
-//                entity.FloatNullKey = entity.IntNullKey;
-//                db.Update<OperationTest>(entity);
+        //        //add一条数据
+        //        OperationTest model = new OperationTest
+        //        {
+        //            IntKey = value,
+        //            StringKey = "AddTest"
+        //        };
+        //        model.IntKey = value;
+        //        db.Add<OperationTest>(model);
 
-//                var entity2 = db.Queryable<OperationTest>().Where(t => t.IntKey == value).FirstOrDefault();
-//                Assert.NotNull(entity2);
-//                Assert.Equal(value, entity2.IntNullKey);
-//                Assert.Equal($"UpdateTest_{value}", entity2.StringKey);
+        //        //插入后查询有一条记录
+        //        var re1 = db.Queryable<OperationTest>().Where(t => t.StringKey.StartsWith("AddTest")).ToList();
+        //        Assert.Single(re1);
+        //        Assert.Equal(value, re1.First().IntKey);
 
-//                //删除数据
-//                db.Delete<OperationTest>(t => t.IntKey == value);
+        //        //查询一条
+        //        var entity = db.Queryable<OperationTest>().Where(t => t.IntKey == value).FirstOrDefault();
+        //        Assert.NotNull(entity);
+        //        Assert.Equal(value, entity.IntKey);
 
-//                //删除后查询没有
-//                var re4 = db.Queryable<OperationTest>().Where(t => t.StringKey.StartsWith("AddTest")).ToList();
-//                Assert.Null(re4);
-//            }
-//        }
+        //        //更新数据
+        //        //entity.Id = value;   //自增的主键不应该被修改,如果用这种方式进行修改，给Id赋值就会导致修改不成功，因为条件是用第一个主键作为标识修改的
+        //        entity.Key2 = value;
+        //        entity.StringKey = $"UpdateTest_{value}";
+        //        entity.IntNullKey = value;
+        //        entity.DateTimeNullKey = DateTime.Now;
+        //        entity.DateNullKey = DateTime.Now.Date;
+        //        entity.DoubleNullKey = entity.IntNullKey;
+        //        entity.FloatNullKey = entity.IntNullKey;
+        //        db.Update<OperationTest>(entity);
 
-//        [Fact]
-//        [Description("持久化测试_默认使用实体主键删除数据")]
-//        public void Persistence_DeleteEntity()
-//        {
-//            using (var db = new DataPreseterDb())
-//            {
-//                int value = 999999;
+        //        var entity2 = db.Queryable<OperationTest>().Where(t => t.IntKey == value).FirstOrDefault();
+        //        Assert.NotNull(entity2);
+        //        Assert.Equal(value, entity2.IntNullKey);
+        //        Assert.Equal($"UpdateTest_{value}", entity2.StringKey);
 
-//                //初次查询没有数据
-//                db.Queryable<OperationTest>().Where(t => t.StringKey.StartsWith("AddTest")).ToList();
+        //        //删除数据
+        //        db.Delete<OperationTest>(t => t.IntKey == value);
 
-//                //add一条数据
-//                OperationTest model = new OperationTest
-//                {
-//                    IntKey = value,
-//                    StringKey = "AddTest"
-//                };
-//                model.IntKey = value;
-//                db.Add<OperationTest>(model);
+        //        //删除后查询没有
+        //        var re4 = db.Queryable<OperationTest>().Where(t => t.StringKey.StartsWith("AddTest")).ToList();
+        //        Assert.Null(re4);
+        //    }
+        //}
 
-//                //插入后查询有一条记录
-//                var re1 = db.Queryable<OperationTest>().Where(t => t.StringKey.StartsWith("AddTest")).ToList();
-//                Assert.Single(re1);
-//                Assert.Equal(value, re1.First().IntKey);
+        //[Fact]
+        //[Description("持久化测试_默认使用实体主键删除数据")]
+        //public void Persistence_DeleteEntity()
+        //{
+        //    using (var db = new DataPreseterDb())
+        //    {
+        //        int value = 999999;
 
-//                var entity = db.Queryable<OperationTest>().Where(t => t.IntKey == value).FirstOrDefault();
-//                Assert.NotNull(entity);
-//                Assert.Equal(value, entity.IntKey);
+        //        //初次查询没有数据
+        //        db.Queryable<OperationTest>().Where(t => t.StringKey.StartsWith("AddTest")).ToList();
 
-//                //删除数据
-//                db.Delete<OperationTest>(entity);
+        //        //add一条数据
+        //        OperationTest model = new OperationTest
+        //        {
+        //            IntKey = value,
+        //            StringKey = "AddTest"
+        //        };
+        //        model.IntKey = value;
+        //        db.Add<OperationTest>(model);
 
-//                //删除后查询没有
-//                var re4 = db.Queryable<OperationTest>().Where(t => t.StringKey.StartsWith("AddTest")).ToList();
-//                Assert.Null(re4);
-//            }
-//        }
-//    }
-//}
+        //        //插入后查询有一条记录
+        //        var re1 = db.Queryable<OperationTest>().Where(t => t.StringKey.StartsWith("AddTest")).ToList();
+        //        Assert.Single(re1);
+        //        Assert.Equal(value, re1.First().IntKey);
+
+        //        var entity = db.Queryable<OperationTest>().Where(t => t.IntKey == value).FirstOrDefault();
+        //        Assert.NotNull(entity);
+        //        Assert.Equal(value, entity.IntKey);
+
+        //        //删除数据
+        //        db.Delete<OperationTest>(entity);
+
+        //        //删除后查询没有
+        //        var re4 = db.Queryable<OperationTest>().Where(t => t.StringKey.StartsWith("AddTest")).ToList();
+        //        Assert.Null(re4);
+        //    }
+        //}
+
+        //[Fact]
+        public void GuidQueryTest()
+        {
+            using (var db = new DataPreseterDb())
+            {
+                var aa = db.Queryable<OperationTest2>().ToList();
+                Assert.NotEqual(Guid.Empty, aa?.FirstOrDefault().Uid);
+            }
+        }
+    }
+}
